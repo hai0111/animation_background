a704f9762a820fa01ad38a941100699237a3c588
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-06-04 11:13:34 +0700

    update timer

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const banner = document.getElementsByClassName("banner")[0]
 const blocks = document.getElementsByClassName("blocks")
 
 for (let i = 0; i < 200; i++) {
-    banner.innerHTML += `<div class="blocks" style="--x: ${i % 20}; --y: ${Math.floor(i / 20)}"></div>`
     const delay = Math.random() * 6
-    blocks[i].style.animationDelay = delay + 's'
-    blocks[i].style.zIndex = Math.ceil(delay)
+    banner.innerHTML +=
+        `<div class="blocks" 
+        style="
+        --x: ${i % 20}; 
+        --y: ${Math.floor(i / 20)}; 
+        --z: ${Math.ceil(delay)}; 
+        --duration: ${delay.toFixed(2) + 's'}">
+        </div>`
 }
+
+const handleInterval = () => {
+    banner.classList.remove("active")
+    setTimeout(() => { banner.classList.add("active") }, 10000)
+}
+
+handleInterval()
+
+setInterval(handleInterval, 20000)
