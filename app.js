7d1272351320eaf33b132c3e25ef3b704eeeb155
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-06-04 07:23:25 +0700

    up

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const banner = document.getElementsByClassName("banner")[0]
 const blocks = document.getElementsByClassName("blocks")
 
-for (let i = 0; i < 400; i++) {
+for (let i = 0; i < 200; i++) {
     banner.innerHTML += `<div class="blocks" style="--x: ${i % 20}; --y: ${Math.floor(i / 20)}"></div>`
     const delay = Math.random() * 6
     blocks[i].style.animationDelay = delay + 's'
