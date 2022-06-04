e1ae81683e499883d2269cc0ac214a5d43dd4918
Nguyễn Văn Hải <haicoi201@gmail.com>
2022-06-04 09:41:23 +0700

    update zindex

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ for (let i = 0; i < 200; i++) {
     banner.innerHTML += `<div class="blocks" style="--x: ${i % 20}; --y: ${Math.floor(i / 20)}"></div>`
     const delay = Math.random() * 6
     blocks[i].style.animationDelay = delay + 's'
+    blocks[i].style.zIndex = Math.ceil(delay)
 }
